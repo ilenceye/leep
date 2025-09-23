b06ad502f7fd0f184e1ddb110ed886c270e36be7
ilenceye <ye66169@gmail.com>
2025-09-23 16:34:34 +0800

    chore: add rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer({
+      gzipSize: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
