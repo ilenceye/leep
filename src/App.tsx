28a15635d74fa0ed14b35f284ff45b949f848885
ilenceye <ye66169@gmail.com>
2025-10-27 21:42:51 +0800

    fix: hide layout in app shortcuts pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,22 @@ import { ROUTES } from "@/constants";
 import { CreateLeepShortcut } from "@/pages/CreateLeepShortcut";
 import { Home } from "@/pages/Home";
 import { Stats } from "@/pages/Stats";
-import { Route } from "wouter";
+import { Route, useLocation } from "wouter";
 
 import { Layout } from "./components/Layout";
 
 export default function App() {
+  const [location] = useLocation();
+  const showLayout = location !== ROUTES.SHORTCUTS;
+
   return (
     <div className="mx-auto h-dvh max-w-md px-4">
-      <Layout>
-        <Route path={ROUTES.HOME} component={Home} />
-        <Route path={ROUTES.STATS} component={Stats} />
-      </Layout>
+      {showLayout && (
+        <Layout>
+          <Route path={ROUTES.HOME} component={Home} />
+          <Route path={ROUTES.STATS} component={Stats} />
+        </Layout>
+      )}
       <Route path={ROUTES.SHORTCUTS} component={CreateLeepShortcut} />
     </div>
   );
